1610fdd0f543f42b77b79996d0a5aa1a59d9b2ff
cj2 <cjgrimes@gmail.com>
2012-08-12 15:29:05 -0600

    additional specs for date/time queries

--- spec/quickbase_mapper/queryable_spec.rb
+++ spec/quickbase_mapper/queryable_spec.rb
@@ -7,14 +7,16 @@ class BasicTestModelQueryable
 end
 
 describe QuickbaseMapper::Queryable do
-    before do
-        QuickbaseMapper.stub(:connections) { {"default" => {"username" => "test", "password" => "password"} } } 
+  before do
+    QuickbaseMapper.stub(:connections) { {"default" => {"username" => "test", "password" => "password"} } } 
+  end
+
+  describe :query do
+    it "should send a doQuery request to the connection's client" do
+        BasicTestModelQueryable.connection.client.should_receive(:doQuery) { [] }
+        BasicTestModelQueryable.where(:name => "steve").to_a
     end
 
-    describe :query do
-        it "should send a doQuery request to the connection's client" do
-            BasicTestModelQueryable.connection.client.should_receive(:doQuery) { [] }
-            BasicTestModelQueryable.where(:name => "steve").to_a
-        end
-    end
+
+  end
 end
--- spec/quickbase_mapper/selectable_spec.rb
+++ spec/quickbase_mapper/selectable_spec.rb
@@ -8,15 +8,22 @@ class BasicTestModelSelectable
 end
 
 describe QuickbaseMapper::Selectable do
-    before do
-        QuickbaseMapper.stub(:connections) { {"default" => {"username" => "test", "password" => "password"} } } 
+  before do
+    QuickbaseMapper.stub(:connections) { {"default" => {"username" => "test", "password" => "password"} } } 
+  end
+
+  describe :build_query do
+    it "should format the string correctly for the QB API" do
+      query = BasicTestModelSelectable.where(:name => "test")
+      query = query.where(:date => 5)
+      query.send(:build_query).should == "{1.EX.'test'}AND{3.EX.'5'}"
     end
 
-    describe :build_query do
-        it "should format the string correctly for the QB API" do
-            query = BasicTestModelSelectable.where(:name => "test")
-            query = query.where(:date => 5)
-            query.send(:build_query).should == "{1.EX.'test'}AND{3.EX.'5'}"
-        end
+    it "should turn date values into milliseconds since epoch" do
+      time = Time.now
+      qb_time = (time.to_f*1000).to_i
+      query = BasicTestModelSelectable.where(:date => Time.now)
+      query.send(:build_query).should == "{3.EX.'#{qb_time}'}"
     end
+  end
 end
